102d4df58123f4eca36ddfa91789ce34d7f2d51a
joseluis <jose.aguilartun@cbtis72.edu.mx>
2023-11-17 16:11:16 -0500

    tabla

---
+++ src/tablaperiodica1/Tablaperiodica1.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tablaperiodica1;
+
+/**
+ *
+ * @author USER
+ */
+public class Tablaperiodica1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ventanadetabla window =new  ventanadetabla();
+        window.setTitle("tabla periodica");
+        window.setVisible(true);
+        dispose();
+    }
+
+    private static void dispose() {
+                                    
+    
+       
+    }
+    
+}
